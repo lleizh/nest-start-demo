7652d2e88aa4798884648b09d70356280b8387ab
lleizh <zhang_leilei@aja-kk.co.jp>
2020-01-09 18:16:27 +0900

    module

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,23 @@
+import { CatsService } from './cats.service';
 import { Controller, Get, Post } from '@nestjs/common';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+
+  constructor(private readonly catsService: CatsService){}
+
   @Post()
-  create(): string {
-    return 'This action adds a new cat';
+  async create() {
+    this.catsService.create({
+      name: 'string',
+      age: 1,
+      breed: 'string'}
+    )
   }
 
   @Get()
-  findAll(): string {
-    return 'This action returns all cats';
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll()
   }
 }
